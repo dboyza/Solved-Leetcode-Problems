462a7f441b0e5cce003218133e0026b132f4530a
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-01-18 19:19:16 -0500

    Time: 12 ms (98.29%), Space: 13.7 MB (34.75%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        mydict = {")":"(", "}": "{", "]": "["}
+        stack = []
+        
+        
+        for char in s:
+            if char in mydict:
+                if stack == [] or stack.pop() != mydict[char]:
+                    return False
+            else:
+                stack.append(char)
+        
+        return len(stack) == 0
