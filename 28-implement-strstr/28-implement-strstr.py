b9af8e606aa76f382d4cef8f49984424c404d2d4
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-01-21 17:54:52 -0500

    Time: 677 ms (21.08%), Space: 14.7 MB (29.44%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -5,11 +5,13 @@ class Solution(object):
         :type needle: str
         :rtype: int
         """
-        """
+        
         if len(needle) == 0:
             return 0
         
         for i in range(len(haystack)-len(needle)+1):
+            if haystack[i] != needle[0]:
+                continue
             if haystack[i:i+len(needle)] == needle:
                 return i
             
@@ -18,3 +20,4 @@ class Solution(object):
         if needle in haystack:
             return haystack.index(needle)
         return -1
+        """
