0429ebf8724f4b840f565cb8507807edab014732
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-01-30 09:49:04 -0500

    Time: 27 ms (39.57%), Space: 13.4 MB (38.90%) - LeetHub

---
+++ 27-remove-element/27-remove-element.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        count = 0
+        start = 0
+        end = len(nums)-1
+        
+        while start <= end:
+            if nums[start] == val:
+                nums[start] = nums[end]
+                nums[end] = nums[start]
+                end -= 1
+                count += 1
+            else:
+                start += 1
+                
+        return start
