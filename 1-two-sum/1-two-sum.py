a469fdab6eeb46c55504739a1c5cd4d7349ce346
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-03-26 14:33:04 -0400

    Time: 68 ms (84.36%), Space: 15.4 MB (16.15%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,30 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        difference = {}
         
-        
-        Time: O(n^2), Space: O(1)
-        
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return None
-        """
-        
-        
-        """
-        Time: O(n), Space: O(n)
-        Use a hashmap (dictionary)
-        Add each element of nums to the dictionary with (key=target-nums[i], value=i)
-        """
-        seen = {}
-        
-        for i in range(len(nums)):
-            if nums[i] in seen:
-                return [seen[nums[i]], i]
-            else:
-                seen[target-nums[i]] = i
+        for i, num in enumerate(nums):
+            if num in difference:
+                return [difference[num], i]
+            difference[target-num] = i
+            
