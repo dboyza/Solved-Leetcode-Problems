d07fcd7f9c552202a5ba2cdd29e477292da6c137
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-03-29 12:35:05 -0400

    Time: 74 ms (5.24%), Space: 13.9 MB (27.51%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.py
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -1,16 +1,13 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        mydict = {")":"(", "}": "{", "]": "["}
+class Solution:
+    def isValid(self, s: str) -> bool:
         stack = []
+        check = {')':'(', '}': '{', ']': '['}
         
         for char in s:
-            if char in mydict:
-                if stack == [] or stack.pop() != mydict[char]:
+            if char in check:
+                if stack == [] or stack.pop() != check[char]:
                     return False
             else:
                 stack.append(char)
+                
         return stack == []
