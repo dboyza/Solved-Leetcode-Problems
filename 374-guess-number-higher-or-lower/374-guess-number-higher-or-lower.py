fa859d0dc6b95c2d413b055c23e8bb67d90ddee3
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-03-29 23:00:44 -0400

    Time: 32 ms (85.51%), Space: 14 MB (16.83%) - LeetHub

--- 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.py
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.py
@@ -12,9 +12,10 @@ class Solution:
         
         while l <= r:
             mid = l+(r-l)//2
-            if guess(mid) == -1:
+            ret = guess(mid)
+            if ret == -1:
                 r = mid-1
-            elif guess(mid) == 1:
+            elif ret == 1:
                 l = mid+1
             else:
                 return mid
