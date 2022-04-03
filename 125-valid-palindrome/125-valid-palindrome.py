aa9cbbe5677fed7ec8c238d12ed1d69cfd32c671
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-04-03 14:42:40 -0400

    Time: 98 ms (15.72%), Space: 14.3 MB (88.11%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -1,5 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        
+        """ NEW STRING METHOD
+        
         new_s = ""
         
         for char in s:
@@ -8,4 +11,22 @@ class Solution:
                     
         return new_s == new_s[::-1]
         
+        """
+        
+        # TWO POINTER METHOD
+        
+        l = 0
+        r = len(s) - 1
+        
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+            
+        return True
         
