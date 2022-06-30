7705e866d3e5116118920732d0db4eeb05b42fc1
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-06-29 21:17:58 -0400

    Time: 4218 ms (6.52%), Space: 27.7 MB (27.99%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.py
+++ 848-shifting-letters/848-shifting-letters.py
@@ -5,6 +5,6 @@ class Solution:
 
         for i in range(len(shifts)-1, -1 , -1):
             sum += shifts[i]
-            new_str = (chr(97  + ((ord(s[i]) + sum - 97) % 26))) + new_str
+            new_str = (chr(97 + ((ord(s[i]) + sum - 97) % 26))) + new_str
 
         return new_str
