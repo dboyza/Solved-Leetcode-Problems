c07bdcdfa8e549076dbd0262c92b5e48f9fd290c
Dylan Boyza <73046516+dboyza@users.noreply.github.com>
2022-06-29 21:17:01 -0400

    Time: 4347 ms (5.78%), Space: 27.7 MB (27.99%) - LeetHub

---
+++ 848-shifting-letters/848-shifting-letters.py
@@ -0,0 +1,10 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        sum = 0
+        new_str = ""
+
+        for i in range(len(shifts)-1, -1 , -1):
+            sum += shifts[i]
+            new_str = (chr(97  + ((ord(s[i]) + sum - 97) % 26))) + new_str
+
+        return new_str
